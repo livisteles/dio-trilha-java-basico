a56c559ae0287949b5df3d18d6d637a9dd0a24c7
liv <livisteles@gmail.com>
2024-05-17 16:24:30 -0300

    algumas anotações da aula de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        // tem que terminar com um L pra que o java intenda que é do tipo long e não do
+        // tipo int
+        float pi = 3.14F; // o tipo float tem que pôr um F no final (nato faz maiúsculo ou minúsculo)
+        double salario = 1411.99;
     }
 }
